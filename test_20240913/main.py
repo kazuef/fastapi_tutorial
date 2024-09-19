05aff88eb29b0d4ae73b345a4ded8c4d6f592034
Kazuki <tokubd127@gmail.com>
2024-09-20 03:39:50 +0900

    クエリパラメータと文字列の検証

--- test_20240913/main.py
+++ test_20240913/main.py
@@ -1,27 +1,12 @@
 from typing import Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.price * item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/")
+async def read_items(q: Union[str, None] = Query(default=None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
