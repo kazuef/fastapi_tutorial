6a1d8d848c2e607593173fa948d4664aa26ffae8
Kazuki <tokubd127@gmail.com>
2024-09-20 00:16:22 +0900

    リクエストボディ

--- test_20240913/main.py
+++ test_20240913/main.py
@@ -1,42 +1,27 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "foo",}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.price * item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-#     item = {"item_id": item_id}
-#     if q:c
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-
-# @app.get("/users/{user_id}/items/{item_id}")
-# async def read_user_item(
-#     user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
-# ):
-#     item = {"item_id": item_id, "owner_id": user_id}
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-
-@app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
