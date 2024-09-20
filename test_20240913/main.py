44e41a0e9176a9dd46b35bf20a98d4debaa99815
Kazuki <tokubd127@gmail.com>
2024-09-20 16:16:06 +0900

    クエリパラメータと文字列の検証

--- test_20240913/main.py
+++ test_20240913/main.py
@@ -5,7 +5,11 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 @app.get("/items/")
-async def read_items(q: Union[str, None] = Query(default=None, max_length=50)):
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None, min_length=3, max_length=50, pattern="^fixedquery$"
+    ),
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
